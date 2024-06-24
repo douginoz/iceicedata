ca66781020ef317f8720df3dfd431356c64ae039
douginoz <inthecentreofitall@gmail.com>
2024-06-24 06:50:26 -0700

    Added functionality to convert timestamp to Unix milliseconds in `convert_wind_speed_to_mps` function.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import pytz
+from datetime import datetime
+
 def split_value_and_unit(value):
     parts = value.split(' ')
     numeric_value = parts[0]
@@ -6,8 +9,13 @@ def split_value_and_unit(value):
 
 def convert_wind_speed_to_mps(speed):
     try:
+        local_tz = pytz.timezone(timezone_str)
+        local_dt = datetime.strptime(timestamp_str, '%m/%d/%Y %I:%M:%S %p')
+        local_dt = local_tz.localize(local_dt, is_dst=None)
+        unix_time_ms = int(local_dt.timestamp() * 1000)
+        return unix_time_ms
         return round(float(speed) * 0.27778, 2)
-    except ValueError:
+    except ValueError as e:
         return speed
 
 def convert_timestamp_to_unix_ms(timestamp_str, timezone_str):
